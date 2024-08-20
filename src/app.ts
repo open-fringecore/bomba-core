82f07b0f8fa638f98ad1d62e3323889b9f1b0198
rifatNR <Rifatnoor92@gmail.com>
2024-08-20 17:59:14 +0600

    initial setup done

---
+++ src/app.ts
@@ -0,0 +1,5 @@
+import { discover } from "@/discover";
+
+console.log("Hello, bomba core is working...");
+
+discover();
---
+++ src/discover.ts
@@ -0,0 +1,3 @@
+export const discover = () => {
+    return "Discovering...";
+};
